7eb723c8519b459ac8363a824dc60be9756548af
CARLOS BUENDIA JIMENEZ <Carlos.Buendia1@alu.uclm.es>
2019-11-16 22:55:55 +0100

    completed some errors. Null pointer exception when calling to Node.addSuccessors, in the las instruction os the UP

--- IntelligentSys/src/intelligentsys/BreathFirst.java
+++ IntelligentSys/src/intelligentsys/BreathFirst.java
@@ -7,7 +7,6 @@ package intelligentsys;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-
 /**
  *
  * @author onsur
@@ -17,34 +16,40 @@ public class BreathFirst {
         int n = 5;
         int nCars = 1;
         //int seed = (int)System.currentTimeMillis();
-        int seed = 693436947;
+        int seed = 2019;
         int[][] maze = Maze.getProblemInstance(n, nCars, seed);
         
         carPosition[] cars;
-        cars = lookupCars(maze);
+        cars = lookupCars(maze,n);
         
-        ArrayList<Node> explored = null;
-        LinkedList<Node> open = null;
+        ArrayList<Node> explored = new ArrayList();
+        System.out.println("llega hasta el arraylist");
+        LinkedList<Node> open = new LinkedList();
         open.add(new Node(maze,cars,n));
         Node node;
         LinkedList<Node> solution = null;
         ArrayList<Node> successor = null;
+        System.out.println("pre while");
         
         while(!open.isEmpty()){
             node = open.getFirst();
-            if(isExplored(explored,node)){
+            
+            if(isExplored(explored,node))
                 if(testGoal(node,n,nCars)){
+                    System.out.println("terminando");
                     solution = recoverPath(node);
                     node.show();
                     showSolution(solution);
+                    break;
                 }
+                System.out.println("añadido sucesor");
                 node.addSuccessors();
                 successor = node.successors;
                 for (Node s : successor) {
                        open.addLast(s);  
                 }
                 explored.add(node);
-            }  
+             
         }
         System.out.println("Failure while doing  breathfirst");
     }
@@ -54,7 +59,7 @@ public class BreathFirst {
     public static boolean testGoal(Node node,int n, int nCars){
         int k = 0;
         for (int i = 0; i < nCars; i++) {
-            if(node.cars[i].y == n - 1)
+            if(node.cars[i].y == n)
                 k++; 
         }
         return(k==nCars);
@@ -62,10 +67,15 @@ public class BreathFirst {
     
     public static boolean isExplored(ArrayList<Node> explored, Node n){
         try{
+            System.out.println("se mete en el explored");
             for (Node node : explored) {
-                if(n.state.equals(node.state))
+                if(n.state.equals(node.state)){
+                    System.out.println("true");
                     return true;
             }
+                
+            }
+            System.out.println("false");
             return false;
         } catch (NullPointerException e){
             return false;
@@ -73,8 +83,8 @@ public class BreathFirst {
     }
     
     
-    private static carPosition[] lookupCars(int[][] maze) {
-        carPosition[] cars = null;
+    private static carPosition[] lookupCars(int[][] maze, int n) {
+        carPosition[] cars = new carPosition[n];
         int k = 0;
         for(int i = 0; i < maze.length; i++){
             if(maze[0][i] > 0){
